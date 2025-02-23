304e898d6c9205f721f24923566a8e9ae3075d76
Fjerez7 <fjerezm07@gmail.com>
2025-02-22 21:06:22 -0500

    Cambios para poder realizar operaciones y mostrarlos en el .csv

--- mini_proyecto_1.py
+++ mini_proyecto_1.py
@@ -3,11 +3,11 @@ import random
 
 # Define operations and their corresponding lambda functions
 operations = {
-    'SUM': None,
-    'SUB': None,
-    'MUL': None,
-    'DIV': None,
-    'POW': None  # limit exponent to avoid very large numbers
+    'SUM': lambda x, y: x + y,
+    'SUB': lambda x, y: x - y,
+    'MUL': lambda x, y: x * y,
+    'DIV': lambda x, y: x / y if y != 0 else None,
+    'POW': lambda x, y: x ** y if y < 10 else x ** 10 # limit exponent to avoid very large numbers
 }
 
 # Generate data
@@ -17,10 +17,11 @@ for _ in range(1000):
     operand_1 = random.randint(1, 1000)
     operand_2 = random.randint(1, 1000)
     if operation == 'POW' and operand_2 < 10: operand_2 = random.randint(1, 15)
-    data.append([operation, operand_1, operand_2])
+    result = operations[operation](operand_1, operand_2)
+    data.append([operation, operand_1, operand_2, result])
 
 # Create DataFrame
-df = pd.DataFrame(data, columns=['operation', 'operand_1', 'operand_2'])
+df = pd.DataFrame(data, columns=['operation', 'operand_1', 'operand_2', 'result'])
 
 # Save to CSV
 csv_path = './data/math_operations.csv'
